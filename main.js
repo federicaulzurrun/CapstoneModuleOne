ae1e77326881fc84a9d285c291fc00142310fb88
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-03-16 20:06:25 -0300

    add desktop view changes

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click'
 /* Speakers Cards */
 const speakers = [
   {
+    grid: 1,
     id: 1,
     img: './assets/1.svg',
     nameS: 'Eunice Goes',
@@ -24,6 +25,7 @@ const speakers = [
     descriptionS: 'Politics at Richmond University, and her area of expertise is ideologies and the role of ideas in the activities of political parties.',
   },
   {
+    grid: 2,
     id: 2,
     img: './assets/2.svg',
     nameS: 'Joern Langhorst',
@@ -31,6 +33,7 @@ const speakers = [
     descriptionS: 'He was educated in Landscape Architecture, Architecture and Urban Planning, and has taught in Landscape Architecture, Architecture.',
   },
   {
+    grid: 1,
     id: 3,
     img: './assets/3.svg',
     nameS: 'Robin Mansell',
@@ -38,6 +41,7 @@ const speakers = [
     descriptionS: 'Her research focuses on technology innovation, digital platform and governance and its socio-political and economic consequences.',
   },
   {
+    grid: 2,
     id: 4,
     img: './assets/4.svg',
     nameS: 'Elzbieta Korolczuk',
@@ -45,6 +49,7 @@ const speakers = [
     descriptionS: ' Associate Professor in sociology working at Södertörn University in Stockholm and American Studies Center, Warsaw University.',
   },
   {
+    grid: 1,
     id: 5,
     img: './assets/5.svg',
     nameS: 'Michał P. Garapich',
@@ -52,6 +57,7 @@ const speakers = [
     descriptionS: 'is an anthropologist and author of an award-winning monograph using intimate ethnography (Dzieci Kazimerza 2019), and over 60 publications.',
   },
   {
+    grid: 2,
     id: 6,
     img: './assets/6.svg',
     nameS: 'William Allchorn',
@@ -65,7 +71,7 @@ const speakersContainer = document.getElementById('speakers-container');
 const cardsCreation = (speakers, containerName) => {
   speakers.forEach((item) => {
     const card = document.createElement('div');
-    card.classList = 'speaker d-flex flex-row';
+    card.classList = 'speaker d-flex';
     card.innerHTML = ` 
       <img class="cards-img" src="${item.img}" alt="">
       <div class="Speaker-info">
